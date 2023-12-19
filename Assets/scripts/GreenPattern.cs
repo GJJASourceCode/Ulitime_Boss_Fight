107103cbcae44b5f23fb1b4050258fa673d56495
gyustur0409 <wnsgud0312@gmail.com>
2023-12-19 21:29:15 +0900

    pls

---
+++ Assets/scripts/GreenPattern.cs
@@ -0,0 +1,260 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenPattern : MonoBehaviour
+{
+    public AudioSource attack1Sound, attack2Sound, jumpAttackSound1, jumpAttackSound2, howlingSound, dashSound;
+    public static bool isAttacking, isDeath;
+    public static int monsterHealth;
+    public static int state;
+    Animator anim;
+    GameObject[] area;
+    public GameObject player, victory;
+    Rigidbody rigid;
+    Vector3 currentVec;
+    bool area1, area2, lookAtPlayer, run;
+    Quaternion rotGoal;
+
+    void Awake()
+    {
+        isAttacking = false;
+        isDeath = false;
+        monsterHealth = 1000;
+        area = new GameObject[4];
+        area[0] = GameObject.Find("Hand_collider");//손 공격범위
+        area[1] = GameObject.Find("Head_collider");//머리 공격범위
+        area[2] = GameObject.Find("jumpRange");//점프 공격범위
+        area[3] = GameObject.Find("chargeRange");//돌진 공격범위
+        area[0].SetActive(false);
+        area[1].SetActive(false);
+        area[2].SetActive(false);
+        area[3].SetActive(false);
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        rigid = GetComponent<Rigidbody>();
+        state = 0;
+        area1 = false;
+        area2 = false;
+        lookAtPlayer = false;
+        run = false;
+        choosePattern();
+    }
+
+    IEnumerator state_0()
+    {
+        float time;
+        yield return new WaitForSeconds(0.2f);
+        lookAtPlayer = true;
+        time = Random.Range(0.4f, 0.8f);
+        yield return new WaitForSeconds(time);
+        if (area1)
+        {
+            if (area2)
+            {
+                state = Random.Range(1, 4);
+            }
+            else
+            {
+                state = 5;
+            }
+        }
+        else
+        {
+            state = 4;
+        }
+        lookAtPlayer = false;
+        choosePattern();
+    }
+    IEnumerator attack1()
+    {
+        anim.SetTrigger("Attack1");
+        yield return new WaitForSeconds(0.7f);
+        attack1Sound.Play();
+        yield return new WaitForSeconds(0.1f);
+        area[0].SetActive(true);
+        isAttacking = true;
+        yield return new WaitForSeconds(0.2f);
+        area[0].SetActive(false);
+        isAttacking = false;
+        yield return new WaitForSeconds(0.5f);
+        state = 0;
+        choosePattern();
+    }
+
+    IEnumerator attack2()
+    {
+        anim.SetTrigger("Attack2");
+        attack2Sound.Play();
+        yield return new WaitForSeconds(0.8f);
+        area[1].SetActive(true);
+        isAttacking = true;
+        yield return new WaitForSeconds(0.1f);
+        area[1].SetActive(false);
+        isAttacking = false;
+        yield return new WaitForSeconds(1.4f);
+        state = 0;
+        choosePattern();
+    }
+    IEnumerator charge()
+    {
+        anim.SetTrigger("scream");
+        howlingSound.Play();
+        yield return new WaitForSeconds(1.4f);
+        anim.SetInteger("run", 1);
+        dashSound.Play();
+        area[3].SetActive(true);
+        isAttacking = true;
+        run = true;
+        lookAtPlayer = true;
+        currentVec = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        yield return new WaitForSeconds(0.25f);
+        currentVec = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        yield return new WaitForSeconds(0.25f);
+        currentVec = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        yield return new WaitForSeconds(0.25f);
+        currentVec = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        yield return new WaitForSeconds(0.25f);
+        currentVec = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        lookAtPlayer = false;
+        yield return new WaitForSeconds(0.75f);
+        area[3].SetActive(false);
+        isAttacking = false;
+        anim.SetInteger("run", 0);
+        run = false;
+        yield return new WaitForSeconds(0.5f);
+        state = 0;
+        choosePattern();
+    }
+    IEnumerator jumpAttack()
+    {
+
+        anim.SetTrigger("jumpAttack");
+        yield return new WaitForSeconds(0.4f);
+        jumpAttackSound1.Play();
+        yield return new WaitForSeconds(1.2f);
+        isAttacking = true;
+        area[2].SetActive(true);
+        jumpAttackSound2.Play();
+        yield return new WaitForSeconds(0.1f);
+        area[2].SetActive(false);
+        isAttacking = false;
+        state = 0;
+        choosePattern();
+    }
+    IEnumerator movetime()
+    {//이동시 2초만 이동
+        yield return new WaitForSeconds(2f);
+        state = 0;
+        choosePattern();
+    }
+    void choosePattern()
+    {
+        Debug.Log("츄즈패턴");
+        Debug.Log(state);
+        if(!isDeath){
+            switch (state)
+        {
+            case 0:
+                StartCoroutine("state_0");//일시정지
+                break;
+            case 1:
+                StartCoroutine("attack1");//attack1 손으로 공격
+                break;
+            case 2:
+                StartCoroutine("attack2");//attack2 꺠물기 
+                break;
+            case 3:
+                StartCoroutine("jumpAttack");//점프
+                break;
+            case 4:
+                StartCoroutine("charge");//charge 돌진 공격   
+                break;
+            case 5:
+                StartCoroutine("movetime");
+                anim.SetInteger("walk", 1);
+                break;
+            default:
+                break;
+        }
+        }
+    }
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.name == "Area1")//바깥원
+        {
+            area1 = true;
+            Debug.Log("접근1");
+        }
+        if (col.gameObject.name == "Area2")//안쪽원
+        {
+            area2 = true;
+            Debug.Log("접근2");
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.name == "Area1")
+        {
+            area1 = false;
+        }
+        if (col.gameObject.name == "Area2")
+        {
+            area2 = false;
+        }
+    }
+    IEnumerator Diiie(){
+        yield return new WaitForSeconds(3f);
+        victory.SetActive(true);
+
+    }
+    void Update()
+    {
+        if(monsterHealth<=0&&!isDeath){
+            isDeath = true;
+            state = 6;
+            monsterHealth = 0;
+            anim.SetTrigger("death");
+            anim.SetInteger("dying",1);
+            StartCoroutine("Diiie");
+        }
+        if(isDeath){
+            monsterHealth = 0;
+        }
+        Vector3 dir = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        Vector3 zero = new Vector3(0f, 0f, 0f);
+        if (run)
+        {
+            rigid.velocity = currentVec.normalized * 20.0f;
+        }
+
+        if (lookAtPlayer)
+        {
+            anim.SetInteger("walk", 1);
+            rotGoal = Quaternion.LookRotation(dir.normalized);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotGoal, 0.02f);
+        }
+        else
+        {
+            anim.SetInteger("walk", 0);
+        }
+
+        if (state == 5)
+        {
+            lookAtPlayer = true;
+            if (area2 == true)
+            {
+                StopCoroutine("movetime");
+                lookAtPlayer = false;
+                state = 0;
+                choosePattern();
+            }
+            rigid.velocity = dir.normalized * 4.0f;
+
+        }
+        if (state == 0 || state == 1 || state == 2 || state == 3 || state == 6)
+        {
+            rigid.velocity = zero * 4.0f;
+        }
+    }
+}
---
+++ Assets/scripts/Playermove.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermove : MonoBehaviour
+{
+    float bbCurrentTime, bbTime;
+    public AudioSource slash1, slash2, bossBattleBGM, defeatBGM, victoryBGM;
+    GameObject body;
+    public GameObject deathtext;
+    public static int slashNum, health;
+    Animator anim;
+    float xInput, zInput, pSpeed, slashTime, slashCurrentTime, rollTime, rollCurrentTime;
+    Vector3 moveVec, point;
+    Vector2 turn;
+    Rigidbody pRigid;
+    bool canRoll, rollEnd;
+    public static bool isSlashing, isRoll, isHited, isDeath, isEnd;
+    void Awake()
+    {
+        isEnd = false;
+        bossBattleBGM.Play();
+        bbTime = 130f;
+        body = GameObject.Find("Armature");
+        pRigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        pSpeed = 4.0f;
+        slashCurrentTime = 0f;
+        rollCurrentTime = 0f;
+        slashTime = 0f;
+        rollTime = 0.9f;
+        isRoll = false;
+        isSlashing = false;
+        isHited = false;
+        isDeath = false;
+        canRoll = true;
+        slashNum = 0;
+        health = 200;
+    }
+    void Update()
+    {
+        Debug.Log(isEnd);
+        bbCurrentTime += Time.deltaTime;
+        if(bbCurrentTime>bbTime){
+            bbCurrentTime =0f;
+            bossBattleBGM.Play();
+        }
+        if(isDeath&&!isEnd){
+            isEnd = true;
+            bossBattleBGM.Stop();
+            defeatBGM.Play();
+        }
+        if(GreenPattern.isDeath&&!isEnd){
+            isEnd = true;
+            bossBattleBGM.Stop();
+            Debug.Log("승리");
+            victoryBGM.Play();
+        }
+        if(health<=0&&!isDeath){
+            anim.SetTrigger("death");
+            isDeath = true;
+            health=0;
+            anim.SetInteger("dying", 1);
+            deathtext.SetActive(true);
+        }
+        turn.x += Input.GetAxis("Mouse X");
+        transform.localRotation = Quaternion.Euler(0, turn.x, 0);
+        xInput = Input.GetAxisRaw("Horizontal");
+        zInput = Input.GetAxisRaw("Vertical");
+        moveVec = new Vector3(xInput, 0f, zInput);
+        slashCurrentTime += Time.deltaTime;
+        rollCurrentTime += Time.deltaTime;
+        if (slashCurrentTime > slashTime)
+        {
+            isSlashing = false;
+        }
+        if (rollCurrentTime > 0.3f)
+        {
+            pSpeed = 4.0f;
+            isRoll = false;
+        }
+        if (rollCurrentTime > 0.7f)
+        {
+            rollEnd = true;
+        }
+        if (rollCurrentTime > rollTime)
+        {
+            canRoll = true;
+        }
+        if(!isDeath){
+            if (Input.GetKey("w"))
+        {
+            pRigid.velocity = transform.forward * pSpeed;
+        }
+        if (Input.GetKey("s"))
+        {
+            pRigid.velocity = -transform.forward * pSpeed;
+        }
+        if (Input.GetKey("d"))
+        {
+            pRigid.velocity = transform.right * pSpeed;
+        }
+        if (Input.GetKey("a"))
+        {
+            pRigid.velocity = -transform.right * pSpeed;
+        }
+        }
+        if (Input.GetMouseButton(0) && !isSlashing && rollEnd && !isDeath)
+        {
+            slashCurrentTime = 0;
+            isSlashing = true;
+            slashNum = Random.Range(1, 3);
+            if (slashNum == 1)
+            {
+                slashTime = 1.2f / 1.5f;
+                anim.SetTrigger("slashing1");
+                slash1.Play();
+            }
+            else if (slashNum == 2)
+            {
+                slashTime = 1.8f / 1.5f;
+                anim.SetTrigger("slashing2");
+                slash2.Play();
+            }
+        }
+        else if (Input.GetKeyDown("left shift") && !isSlashing && canRoll && !isDeath)
+        {
+            rollCurrentTime = 0;
+            isRoll = true;
+            rollEnd = false;
+            canRoll = false;
+            pSpeed = 10f;
+            anim.SetTrigger("roll");
+        }
+        if (Mathf.Abs(xInput) > Mathf.Epsilon || Mathf.Abs(zInput) > Mathf.Epsilon)
+        {
+            if(!isDeath){
+                anim.SetInteger("animState", 1);
+            }
+        }
+        else
+        {
+            if(!isDeath){
+                anim.SetInteger("animState", 0);
+            }
+        }
+    }
+}
