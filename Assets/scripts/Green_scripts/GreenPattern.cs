b172b00a84292ecb4916e103c30a0be35fb22ef5
gyustur0409 <wnsgud0312@gmail.com>
2023-12-24 22:53:20 +0900

    ,

--- Assets/scripts/Green_scripts/GreenPattern.cs
+++ Assets/scripts/Green_scripts/GreenPattern.cs
@@ -13,12 +13,12 @@ public class GreenPattern : MonoBehaviour
     public GameObject player, victory, BackJumpPos;
     Rigidbody rigid;
     Vector3 currentVec, BackJumpVec;
-    bool lookAtPlayer, run, isBackJump;
+    bool lookAtPlayer, run, isBackJump, isMove;
     Quaternion rotGoal;
 
     int isRight;
 
-    
+
     private Vector3 velocity = Vector3.zero;
 
     void Awake()
@@ -45,34 +45,44 @@ public class GreenPattern : MonoBehaviour
     }
     IEnumerator state_0()
     {
-        if(Green_Area_Detect.whichArea==1){
+        yield return new WaitForSeconds(0.1f);
+        if (Green_Area_Detect.whichArea == 1)
+        {
             state = 1;
         }
-        else if(Green_Area_Detect.whichArea==2){
+        else if (Green_Area_Detect.whichArea == 2)
+        {
             state = Random.Range(2, 6);
         }
-        else if(Green_Area_Detect.whichArea==3){
-            state = Random.Range(6,8);
+        else if (Green_Area_Detect.whichArea == 3)
+        {
+            state = Random.Range(6, 8);
         }
-        else if(Green_Area_Detect.whichArea==4){
+        else if (Green_Area_Detect.whichArea == 4)
+        {
             state = 8;
         }
         choosePattern();
         yield return 0;
     }
-    IEnumerator BackJump(){
+    IEnumerator BackJump()
+    {
         anim.SetTrigger("BackJump");
         BackJumpVec = BackJumpPos.transform.position;
         yield return new WaitForSeconds(0.6f);
         isBackJump = true;
         yield return new WaitForSeconds(0.4f);
         isBackJump = false;
+        state = 0;
+        choosePattern();
     }
-    /*IEnumerator move(){
+    IEnumerator move()
+    {
+
         yield return new WaitForSeconds(2f);
         state = 0;
         choosePattern();
-    }*/
+    }
 
     /*IEnumerator attack2()
     {
@@ -142,93 +152,115 @@ public class GreenPattern : MonoBehaviour
     }*/
     void choosePattern()
     {
-        if(!isDeath){
-            switch (state)
+        Debug.Log("츄즈");
+        if (!isDeath)
         {
-            case 0:
-                StartCoroutine("state_0");//일시정지
-                break;
-            case 1:
-                //attack1 뒤로 회피
-
-                break;
-            case 2:
-                //attack2 물기
-                break;
-            case 3:
-                //박치기
-                break;
-            case 4:
-                //한번 패기   
-                break;
-            case 5:
-                //두번 패기
-                break;
-            case 6:
-                StartCoroutine("move");
-                break;
-            case 7:
-                isRight = Random.Range(0,2);
-                StartCoroutine("move");
-                //옆으로
-                break;
-            case 8:
+            switch (state)
+            {
+                case 0:
+                    StartCoroutine("state_0");//일시정지
+                    break;
+                case 1:
+                    // 뒤로 회피
+                    StartCoroutine("BackJump");
+                    break;
+                case 2:
+                    //attack2 물기
+                    Debug.Log("물기");
+                    StartCoroutine("state_0");
+                    break;
+                case 3:
+                    //박치기
+                    Debug.Log("박치기");
+                    StartCoroutine("state_0");
+                    break;
+                case 4:
+                    //한번 패기
+                    Debug.Log("한번패기");
+                    StartCoroutine("state_0");
+                    break;
+                case 5:
+                    Debug.Log("두번패기");
+                    StartCoroutine("state_0");
+                    break;
+                case 6:
+                    StartCoroutine("move");
+                    break;
+                case 7:
+                    isRight = Random.Range(0, 2);
+                    StartCoroutine("move");
+                    //옆으로
+                    break;
+                case 8:
+                    StartCoroutine("state_0");
+                    break;
+                //돌진
                 //비고
-            default:
-                break;
-        }
+                default:
+                    break;
+            }
         }
     }
 
-    IEnumerator Diiie(){
+    IEnumerator Diiie()
+    {
         yield return new WaitForSeconds(3f);
         victory.SetActive(true);
 
     }
     void Update()
     {
-        Debug.Log(Green_Area_Detect.whichArea);
-        if(isBackJump){
+        Debug.Log(state);
+        if (isBackJump)
+        {
             transform.position = Vector3.Lerp(transform.position, BackJumpVec, 0.02f);
         }
-        if(monsterHealth<=0&&!isDeath){
+        if (monsterHealth <= 0 && !isDeath)
+        {
             isDeath = true;
             state = 8;
             monsterHealth = 0;
             anim.SetTrigger("death");
-            anim.SetInteger("dying",1);
+            anim.SetInteger("dying", 1);
             StartCoroutine("Diiie");
         }
-        if(isDeath){
+        if (isDeath)
+        {
             monsterHealth = 0;
         }
         Vector3 dir = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
         Vector3 zero = new Vector3(0f, 0f, 0f);
-        if (state==6)
+        if (state == 6)
         {
-            if(Green_Area_Detect.whichArea!=3){
+            if (Green_Area_Detect.whichArea != 3)
+            {
                 StopCoroutine("move");
+                anim.SetInteger("walk", 0);
                 state = 0;
                 choosePattern();
             }
             lookAtPlayer = true;
-            //걷기모션 설정
+            anim.SetInteger("walk", 1);
             rigid.velocity = dir.normalized * 4.0f;
         }
 
-        if (state==7)
+        if (state == 7)
         {
-            if(Green_Area_Detect.whichArea!=3){
+            anim.SetInteger("walk", 1);
+            if (Green_Area_Detect.whichArea != 3)
+            {
                 StopCoroutine("move");
                 //걷기모션 죽이기
                 state = 0;
                 choosePattern();
             }
-            if(isRight==1){
+            if (isRight == 1)
+            {
                 //오른쪽걷기모션 설정
                 rigid.velocity = transform.right * 4.0f;
             }
-            else if(isRight==0){
+            else if (isRight == 0)
+            {
                 //왼쪽쪽걷기모션 설정
                 rigid.velocity = transform.right * -4.0f;
             }
--- Assets/scripts/Green_scripts/Green_Area_Detect.cs
+++ Assets/scripts/Green_scripts/Green_Area_Detect.cs
@@ -6,38 +6,39 @@ public class Green_Area_Detect : MonoBehaviour
 {
     public static int whichArea;
     bool area1, area2, area3;
+    public GameObject boss;
     // Start is called before the first frame update
     void Start()
     {
-        whichArea=0;
+        whichArea = 0;
     }
 
-void OnTriggerStay(Collider col)
+    void OnTriggerStay(Collider col)
     {
-        if (col.gameObject.name == "Area1")
+        if (col.gameObject.name == "Ar1")
         {
             area1 = true;
         }
-        if (col.gameObject.name == "Area2")
+        if (col.gameObject.name == "Ar2")
         {
             area2 = true;
         }
-        if (col.gameObject.name == "Area3")
+        if (col.gameObject.name == "Ar3")
         {
             area3 = true;
         }
     }
     void OnTriggerExit(Collider col)
     {
-        if (col.gameObject.name == "Area1")
+        if (col.gameObject.name == "Ar1")
         {
             area1 = false;
         }
-        if (col.gameObject.name == "Area2")
+        if (col.gameObject.name == "Ar2")
         {
             area2 = false;
         }
-        if (col.gameObject.name == "Area3")
+        if (col.gameObject.name == "Ar3")
         {
             area3 = false;
         }
@@ -45,18 +46,22 @@ void OnTriggerStay(Collider col)
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(area1+", "+area2+", "+area3);
-        if(area1){
+        if (area1)
+        {
             whichArea = 1;
         }
-        if(!area1&&area2){
+        if (!area1 && area2)
+        {
             whichArea = 2;
         }
-        if(!area2&&area3){
+        if (!area2 && area3)
+        {
             whichArea = 3;
         }
-        if(!area3){
+        if (!area3)
+        {
             whichArea = 4;
         }
+        transform.position = boss.transform.position;
     }
 }
